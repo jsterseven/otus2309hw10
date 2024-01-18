a13d885730c16297176f4f582c4db0df3da65824
jster42 <slavikabs@gmail.com>
2024-01-18 16:57:49 +0300

    Added jest config

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+    testMatch: ['**/specs/**.spec.js'],
+    "transform": {
+      "^.+\\.[t|j]sx?$": "babel-jest"
+    },
+    reporters: [
+      "default", 
+      [
+        "jest-html-reporters", {
+          "publicPath": "./jest-html-report",
+          "filename": "report.html"
+        }
+      ]
+    ]
+  }
